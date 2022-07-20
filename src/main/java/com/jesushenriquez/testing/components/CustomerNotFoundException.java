7acf8957162ae34b31f5fd78e077b9391ed96312
Jesus Henriquez <henriquezfilardo@icloud.com>
2022-07-19 21:53:03 -0500

    feature: add exception

---
+++ src/main/java/com/jesushenriquez/testing/components/CustomerNotFoundException.java
@@ -0,0 +1,19 @@
+package com.jesushenriquez.testing.components;
+
+import lombok.Getter;
+
+import java.io.Serial;
+
+@Getter
+public class CustomerNotFoundException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = -7005849637616672680L;
+    private final String errorMessage;
+
+    public CustomerNotFoundException(String errorMessage) {
+        super(errorMessage);
+        this.errorMessage = errorMessage;
+    }
+
+}

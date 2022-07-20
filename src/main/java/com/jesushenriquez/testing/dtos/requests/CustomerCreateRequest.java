c2ee52cd86b03b5a71138abe8cc4a584f0948c64
Jesus Henriquez <henriquezfilardo@icloud.com>
2022-07-19 23:44:13 -0500

    feature: add JsonProperty in request dto

--- src/main/java/com/jesushenriquez/testing/dtos/requests/CustomerCreateRequest.java
+++ src/main/java/com/jesushenriquez/testing/dtos/requests/CustomerCreateRequest.java
@@ -1,5 +1,6 @@
 package com.jesushenriquez.testing.dtos.requests;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.io.Serial;
@@ -14,7 +15,12 @@ public class CustomerCreateRequest implements Serializable {
     @Serial
     private static final long serialVersionUID = 8084395064601241532L;
 
+    @JsonProperty("firstName")
     private String firstName;
+
+    @JsonProperty("lastName")
     private String lastName;
+
+    @JsonProperty("email")
     private String email;
 }
